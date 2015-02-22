6d012f39e9e02300fb464e45130d27a7385da08e
Ahmad Hakeem <ahmadhakeem@email.arizona.edu>
2015-02-22 14:45:23 -0700

    Add some additional R-code, up till boxplot

--- ggplot_example.R
+++ ggplot_example.R
@@ -1,3 +1,25 @@
 # ggplot learning script
 # Ahmad Hakeem ahmadhakeem@email.arizona.edu
 # Feb 22 2015
+
+#install.packages("ggplot2", dependencies = TRUE)
+#install.packages("plyr")
+#install.packages("ggthemes")
+#install.packages("reshape2")
+
+require(ggplot2)
+require(plyr)
+require(ggthemes)
+require(reshape2)
+
+head(iris)
+iris[1:2, ]
+#ddply(iris, .(Species), summarize, mean.Sep.Wid = mean())
+
+myplot <- ggplot(iris, aes(Sepal.Length, Sepal.Width, color=Species))
+myplot + geom_point(aes(shape=Species), size=3)
+
+head(d2)
+d2 <- diamonds[sample(1:dim(diamonds)[1], 1000), ]
+d2plot <- ggplot(d2, aes(carat, price, color=color))
+d2plot + geom_point()
